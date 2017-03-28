9c657ef5731d6be44f0af3e0496fbfd89c4e5520
Andy Do <iosinutvbox@users.noreply.github.com>
2017-03-27 21:12:23 -0400

    Update plist.py

--- plist.py
+++ plist.py
@@ -11,8 +11,8 @@ addons_folder = xbmc.translatePath('special://home/addons')
 image         = xbmc.translatePath(os.path.join(path, "icon.png"))
 
 plugin         = Plugin()
-addon          = xbmcaddon.Addon("plugin.video.thongld.vnplaylist")
-pluginrootpath = "plugin://plugin.video.thongld.vnplaylist"
+addon          = xbmcaddon.Addon("plugin.video.iosinu.tv.box")
+pluginrootpath = "plugin://plugin.video.iosinu.tv.box"
 http           = httplib2.Http(cache, disable_ssl_certificate_validation=True)
 query_url      = "https://docs.google.com/spreadsheets/d/{sid}/gviz/tq?gid={gid}&headers=1&tq={tq}"
 sheet_headers  = {

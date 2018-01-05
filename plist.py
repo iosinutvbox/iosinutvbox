d9b1582de5c1e0f9603c7aeba23801b9f05e6898
Andy Do <iosinutvbox@users.noreply.github.com>
2018-01-04 20:23:33 -0500

    Update plist.py

--- plist.py
+++ plist.py
@@ -150,8 +150,8 @@ def getItems(url_path="0", tq="select A,B,C,D,E"):
 		if "plugin://" in item["path"]:
 			if "install-repo" in item["path"]:
 				item["is_playable"] = False
-			elif re.search("plugin.video.HieuHien.vn/(.+?)/.+?\://", item["path"]):
-				match = re.search("plugin.video.HieuHien.vn(/.+?/).+?\://", item["path"])
+			elif re.search("plugin.video.GrounDIY.Media.IPTV/(.+?)/.+?\://", item["path"]):
+				match = re.search("plugin.video.GrounDIY.Media.IPTV(/.+?/).+?\://", item["path"])
 				tmp = item["path"].split(match.group(1))
 				tmp[-1] = urllib.quote_plus(tmp[-1])
 				item["path"] = match.group(1).join(tmp)
